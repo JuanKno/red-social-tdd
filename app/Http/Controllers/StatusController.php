a3389b21381c0a010ed26815462803534bb0c048
Juan Esteban <juanes0202@gmail.com>
2021-02-21 15:28:37 -0500

    pruebas de creación de status

---
+++ app/Http/Controllers/StatusController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Status;
+use Illuminate\Http\Request;
+
+class StatusController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'body' => 'required|min:5'
+        ]);
+
+        $status = Status::create([
+            'body' => $request->body,
+            'user_id' => auth()->id()
+        ]);
+
+        return response()->json([
+            'body' => $status->body
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\Status  $status
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Status $status)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Model\Status  $status
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Status $status)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\Status  $status
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Status $status)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\Status  $status
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Status $status)
+    {
+        //
+    }
+}
---
+++ tests/Feature/CreateStatusesTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateStatusesTest extends TestCase
+{
+    use RefreshDatabase;
+    /**    
+     * @test
+     */
+    public function guest_users_can_not_create_statuses()
+    {
+        // $this->withoutExceptionHandling();
+
+        $response = $this->post(route('status.store'), [
+            'body' => 'My first status'
+        ]);
+
+        $response->assertRedirect('login');
+    }
+
+
+
+    /**    
+     * @test
+     */
+    public function an_authenticated_user_can_create_statuses()
+    {
+        // $this->withoutExceptionHandling();
+
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $response = $this->postJson(route('status.store'), [
+            'body' => 'My first status'
+        ]);
+
+        $response->assertJson([
+            'body' => 'My first status'
+        ]);
+
+        $this->assertDatabaseHas('statuses', ['body' => 'My first status', 'user_id' => $user->id]);
+    }
+
+    /**    
+     * @test
+     */
+    public function a_status_require_a_body()
+    {
+        // $this->withoutExceptionHandling();
+
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $response = $this->postJson(route('status.store'), [
+            'body' => ''
+        ]);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonStructure([
+            'message', 'errors' => ['body']
+        ]);
+    }
+
+    /**    
+     * @test
+     */
+    public function a_status_body_require_a_minimum_length()
+    {
+        // $this->withoutExceptionHandling();
+
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $response = $this->postJson(route('status.store'), [
+            'body' => 'adf'
+        ]);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonStructure([
+            'message', 'errors' => ['body']
+        ]);
+    }
+}
