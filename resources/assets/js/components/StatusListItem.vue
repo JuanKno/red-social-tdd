ab249b5bd6a69d4e7fec60aa1838d174d6b52620
Juan Esteban <juanes0202@gmail.com>
2021-03-06 00:02:41 -0500

    diseñando listado de comentarios

--- resources/assets/js/components/StatusListItem.vue
+++ resources/assets/js/components/StatusListItem.vue
@@ -24,15 +24,46 @@
         <i class="far fa-thumbs-up"></i>
         <span dusk="likes-count">{{ status.likes_count }}</span>
       </div>
-      <form @submit.prevent="addComment">
-        <textarea name="comment" v-model="newComment"></textarea>
-        <button dusk="comment-btn">Enviar</button>
-      </form>
-
-      <div v-for="comment in comments" :key="comment.id">
-        {{ comment.body }} <br>
-        {{ comment.user_name }}
+    </div>
+    <div class="card-footer">
+      <div v-for="comment in comments" :key="comment.id" class="mb-3">
+        <img
+          width="34px"
+          :src="comment.user_avatar"
+          class="rounded shadow-sm float-left mr-2"
+          :alt="comment.user_name"
+        />
+        <div class="card border-0 shadow-sm">
+          <div class="card-body p-2 text-secondary">
+            <a href="#">
+              <strong> {{ comment.user_name }}</strong>
+            </a>
+            {{ comment.body }}
+          </div>
+        </div>
       </div>
+      <form @submit.prevent="addComment" v-if="isAuthenticated">
+        <div class="d-flex align-items-center">
+          <img
+            width="34px"
+            src="https://www.beahero.gg/wp-content/uploads/2019/07/Re-Zero-Rem.jpg"
+            class="rounded shadow-sm mr-2"
+            :alt="currentUser.name"
+          />
+          <div class="input-group">
+            <textarea
+              v-model="newComment"
+              name="comment"
+              class="form-control border-0 shadow-sm"
+              rows="1"
+              placeholder="Escribe un comentario..."
+            ></textarea>
+            <div class="input-group-append">
+              <button class="btn btn-primary" dusk="comment-btn">Enviar</button>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   </div>
 </template>
